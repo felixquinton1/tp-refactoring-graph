e265cd7fa81b237f50d0b0e77ed2697b853c40e3
felix_quinton <felix.quinton@hotmail.fr>
2021-01-06 13:59:08 +0100

    0.4

---
+++ src/main/java/org/acme/graph/model/Path.java
@@ -0,0 +1,16 @@
+package org.acme.graph.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Path {
+	private List<Edge> listEdge = new ArrayList<>();
+	
+	public Path(){}
+	
+	public List<Edge> getListEdge() {
+		return listEdge;
+	}
+	
+	
+}
